3e23d31107ab7449f4ffea0cad229e5f89a1cc0c
Mark Wilde <mwilde@ingenium-ids.org>
2014-09-01 12:55:58 +0100

    Initial layout for smtp service class

---
+++ net/smtp/Service.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace li3_mail\net\smtp;
+
+use lithium\core\Libraries;
+use lithium\core\ClassNotFoundException;
+use lithium\core\NetworkException;
+
+class Service extends \lithium\core\Object {
+
+}
+
+?>
---
+++ tests/cases/net/smtp/ServiceTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace li3_mail\tests\cases\net\smtp;
+
+use lithium\core\NetworkException;
+
+class ServiceTest extends \lithium\test\Unit {
+
+}
+
+?>
