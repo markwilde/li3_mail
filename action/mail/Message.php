9b1671168cceb4fc1e3a42278a9975ff32b80d07
Mark Wilde <mwilde@ingenium-ids.org>
2014-09-01 12:56:23 +0100

    Initial layout of message class

--- action/mail/Message.php
+++ action/mail/Message.php
@@ -2,9 +2,30 @@
 
 namespace li3_mail\action\mail;
 
+use BadMethodCallException;
+
 class Message extends \lithium\core\Object {
 
+	protected $_autoConfig = array(
+		'queue'
+	);
 
+	public $to = null;
+
+	public $from = null;
+
+	public $subject = null;
+
+	public $body = null;
+
+	public function send() {
+		$method = __FUNCTION__;
+		if ($transport = $this->_transport) {
+			return call_user_func_array(array(&$transport, $method), array($this));
+		}
+		$message = "No queue bound to call `{$method}`.";
+		throw new BadMethodCallException($message);
+	}
 
 }
 
